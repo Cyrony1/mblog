3fdf4f6f24369ef84bde57e8448db3d0e6869595
Cyrony <cyrony@hotmail.com>
2023-08-23 18:26:42 +0800

    change the view and others

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from mysite.models import Post
+from datetime import datetime
 
-# Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request, "index.html", locals())
